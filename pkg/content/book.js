c6fbd906c10da849969a5f5e97137a315272d012
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-03-28 10:08:24 -0500

    Updated book logic

--- pkg/content/book.js
+++ pkg/content/book.js
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import yaml from 'js-yaml'
 import figlet from 'figlet'
 import slugify from 'slugify'
 import gradient from 'gradient-string'
@@ -12,21 +13,24 @@ export const configBook = program => {
   program.command('book')
     .description('Book')
     .argument('[topic]', 'What topic do you want to discuss?', 'How to write a book')
-    .option('-c, --chapters <number>', 'the number of Chapters in the book', '20')
-    .option('-w, --words <words>', 'How many words should be generated per chapter?', '2000')
+    .option('-c, --chapters <chapters>', 'the number of Chapters in the book', '20')
+    .option('-s, --sections <sections>', 'the number of Sections in each Chapter', '5')
+    .option('-w, --words <words>', 'How many words should be generated per section?', '2000')
     .option('-o, --output <path>', 'What path should the content be output?', isNext ? path.join('pages/posts') : '_posts')
     .action((str, options) => args = { contentType: 'Book', topic: str, ...options })
 }
 
-export const generateBook = async ({chapters, number, topic, output, words}) => {
+export const generateBook = async ({chapters, sections, topic, output, words}) => {
 
   const topicSpinner = createSpinner(`Asking GPT to generate topics on "${topic}" ...`).start()
 
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: [
-          {'role': 'system', 'content': 'You are a helpful assistant that only responds in YAML format arrays.'},
-          {'role': 'user', 'content': `Respond with a list of ${number} possible titles of ${contentType} for the topic "${topic}". Do not count the items in the list.`},
+          // {'role': 'system', 'content': 'You are a helpful assistant that only responds in YAML format arrays.' },
+          // {'role': 'user', 'content': `Respond with ${chapters} chapter titles for a book on the topic "${topic}". `},
+          {'role': 'system', 'content': 'You are a helpful assistant that only responds in Markdown format.' },
+          {'role': 'user', 'content': `Respond with a Table of Contents for a book on the topic "${topic}" with ${chapters} chapters, and with at least ${sections} sections per chapter. Do not number the parts, chapters or sections.`}
       ]
     })
 
@@ -34,24 +38,50 @@ export const generateBook = async ({chapters, number, topic, output, words}) => 
 
   const content = response.data.choices[0].message.content
 
-  const items = createToc({topic, content, output})
+  fs.writeFileSync(`${output}/outline.md`, yaml.dump(content))
 
-  const contentSpinner = createSpinner('Asking GPT to generate content for each topic ...').start()
+  // const items = createToc({topic, content, output})
 
-  await Promise.all(items.map(async (item) => {
-    return openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-            {'role': 'system', 'content': 'You are a helpful assistant that only responds in Markdown format that starts with title `# `'},
-            {'role': 'user', 'content': `Respond with a ${words} word blog post on the topic "${item}" which will be posted on a blog about ${topic}.`},
-        ]
-      }).then(itemResponse => {
-        const itemContent = itemResponse.data.choices[0].message.content
-        fs.writeFileSync(`${output}/${slugify(item, { strict: true })}.md`, itemContent)
-  })}))
+  // const contentSpinner = createSpinner('Asking GPT to generate sections for each chapter ...').start()
 
-  contentSpinner.success({ text: 'GPT has completed generating all of the content for each topic!'})
+  // const chapterSections = await Promise.all(items.map(async (item) => {
+  //   return openai.createChatCompletion({
+  //     model: 'gpt-3.5-turbo',
+  //     messages: [
+  //           // {'role': 'system', 'content': 'You are a helpful assistant that only responds in YAML format arrays.' },
+  //           {'role': 'system', 'content': 'You are a helpful assistant that only responds in Markdown format.' },
+  //           {'role': 'user', 'content': `Respond with ${sections} section titles in the "${item}" chapter for a book with the following chapters: \n\n${content} `},
+  //       ]
+  //     }).then(itemResponse => {
+  //       const itemContent = itemResponse.data.choices[0].message.content
+  //       // fs.writeFileSync(`${output}/${slugify(item, { strict: true })}.md`, itemContent)
+  //       return itemContent
+  // })}))
 
-  console.log(gradient.pastel.multiline(figlet.textSync('Success!')))
+  // fs.writeFileSync(`${output}/sections.md`, yaml.dump(chapterSections))
+
+  // contentSpinner.success({ text: 'GPT has completed generating all of the section titles for each chapter!'})
+
+  // console.log(gradient.pastel.multiline(figlet.textSync('Success!')))
 
 }
+
+const systemPrompt = `You are a helpful assistant that only responds in following YAML format:
+
+
+\`\`\`yaml
+Book:
+  title: 
+  description: 
+  chapters: [Chapter]
+
+Chapter:
+  title:
+  description:
+  sections: [Section]
+
+Section:
+  title:
+  description:
+\`\`\`
+`
